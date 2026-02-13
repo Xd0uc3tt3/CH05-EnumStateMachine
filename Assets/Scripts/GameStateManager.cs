48b041d81d43b46dc56a3b7ec8af06ea11265746
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-02-13 15:44:40 -0400

    Set up GameStateManager & UIManager Functionality

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,15 +1,79 @@
 using UnityEngine;
 
+public enum GameState
+{
+    None,
+    Init,
+    MainMenu,
+    Gameplay,
+    Paused
+
+}
+
 public class GameStateManager : MonoBehaviour
 {
-    public enum GameState
+
+    [SerializeField] UIManager UIManager;
+    public GameState currentState { get; private set; }
+    public GameState previousState { get; private set; }
+
+    [SerializeField] private string currentActiveState;
+    [SerializeField] private string previousActiveState;
+
+
+
+    private void Start()
     {
-        MainMenu,
-        Gameplay,
-        Pause,
-        Settings,
-        GameOver
+        UIManager = ServiceHub.Instance.UIManager;
+        SetState(GameState.Init);
     }
 
-    public GameState CurrentState { get; private set; }
+    public void SetState(GameState newState)
+    {
+        if (currentState == newState)
+        {
+            return;
+        }
+
+        previousState = currentState;
+        currentState = newState;
+
+        currentActiveState = currentState.ToString();
+        previousActiveState = previousState.ToString();
+
+        OnGameStateChanged(previousState, currentState);
+    }
+
+    private void OnGameStateChanged(GameState previousState, GameState newState)
+    {
+        switch (newState)
+        {
+            case GameState.Init:
+                Debug.Log("GameState Changed to Init");
+                SetState(GameState.MainMenu);
+                break;
+
+            case GameState.MainMenu:
+                Debug.Log("GameState Changed to MainMenu");
+                UIManager.ShowMainMenuUI();
+                break;
+
+            case GameState.Gameplay:
+                Debug.Log("GameState Changed to GamePlay");
+                UIManager.ShowGameplayUI();
+                break;
+
+            case GameState.Paused:
+                Debug.Log("GameState Changed to Paused");
+                UIManager.ShowPausedUI();
+                break;
+
+            default:
+                break;
+
+
+        }
+    }
+
+
 }
